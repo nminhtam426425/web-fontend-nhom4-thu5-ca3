f23b5060000d693b3ce01c653212791d6123d2fa
nminhtam426425 <nminhtam426425@gmail.com>
2026-02-27 16:07:40 +0700

    Update index.js

--- src/res/component/index.js
+++ src/res/component/index.js
@@ -2,7 +2,7 @@ import TableUsers from "./table/TableUsers";
 import FormAddUser from "./Form/FormAddUser";
 
 const apiUserService = ({
-  baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:8081/users',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8081/users',
 })
 
 export {TableUsers,FormAddUser,apiUserService}
