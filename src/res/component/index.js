92dd210c40498a5a9ae32b7ddaf6233ca2ca01d0
nminhtam426425 <nminhtam426425@gmail.com>
2026-02-27 15:50:30 +0700

    update url api

--- src/res/component/index.js
+++ src/res/component/index.js
@@ -2,7 +2,7 @@ import TableUsers from "./table/TableUsers";
 import FormAddUser from "./Form/FormAddUser";
 
 const apiUserService = ({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081/users',
-});
+  baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:8081/users',
+})
 
 export {TableUsers,FormAddUser,apiUserService}
